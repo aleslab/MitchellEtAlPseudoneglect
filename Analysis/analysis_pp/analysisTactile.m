f31f4025f3febbde3b2c8d448cc479ce0581dd40
Alex Mitchell <agm26@st-andrews.ac.uk>
2018-11-20 15:18:46 +0000

    Tactile analysis!
    Grouping analyses into one file and deleting empty TR2 file

--- Analysis/analysis_pp/analysisTactile.m
+++ Analysis/analysis_pp/analysisTactile.m
@@ -1,20 +1,35 @@
-%% Analysing Tactile Rod 2AFC task - AG.Mitchell 01.10.18
-%% Data analysis script
-% This script takes the data from the TR2 task and calcualtes the
-% percentage of times participants said that the top line is longer
-% This is then calculated against the amount (in mm) of shift in the top
-% line (right or left)
-% Psychometric curve calculated, and the point of subjective equality (PSE)
-% used as a measure of bias in each participant 
-% Psychometric function calculated using palamedes toolbox (type used:
-% cumulative normal)
+%% Analysis for Tactile Rod Bisection - AG.Mitchell 01.10.18
+%% Runs analysis on TRB task
+% Loads in the data for the task and averages the response across all
+% variables
+% Then calculates bias
+% Then calculates bias relative to line length and shift into L/R hemifield
 
-%% Note
-% Lapse rate assumed 0 here, but will be calculated more accurately into
-% paradigm 
-
-%% Importing data
+%% Importing the data
 ppID = input('Participant ID? ', 's'); %for use when navigating files
+matfilename = sprintf('TRB_analysis%s.mat', ppID);
+
+data = struct;
+data.size = [];
+data.side = [];
+data.response = [];
+data.matrix = [];
+
+[Data,Text] = xlsread('TrialMatrix_TRB'); %importing trial information for landmark tasks
+data.size = Data(:, ismember(Text(1,:), 'size'));
+data.side = Data(:, ismember(Text(1,:), 'shift')); %2deg left = 1, middle = 0, 2deg right = 2
+data.response = Data(:, ismember(Text(1,:), 'response')); %in mm, relative to length of line
+
+%% Finding error 
+data.size = data.size*100; %finding actual line length in mm to calculate error
+data.error = data.response - data.size/2; %calculating data error in mm
+% data matrix for later analyses
+data.matrix = [data.size, data.side, data.response, data.error];
+data.errormean = nanmean(data.error); %average results in mm
+data.errorsd = nanstd(data.error);
+
+save(matfilename);
+
 matfilename = sprintf('TR2_analysis%s.mat', ppID);
 
 data = struct;
