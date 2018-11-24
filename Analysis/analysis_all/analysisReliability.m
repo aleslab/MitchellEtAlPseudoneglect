3499e3b0051a220247dc3ee7fd35d8b15c046682
Alex Mitchell <agm26@st-andrews.ac.uk>
2018-11-24 12:53:43 +0000

    Directory info and variables

--- Analysis/analysis_all/analysisReliability.m
+++ Analysis/analysis_all/analysisReliability.m
@@ -8,3 +8,24 @@
 % (stimulus asymmetry when the participant thinks the line is bisected down
 % the midline) for LM tasks and uses Cronbach's alpha to assess whether the
 % data is consistent across sessions and modalities
+
+%% Loading data for each session
+nParticipants = 1:17;
+for p = 1:length(nParticipants)
+    ppID = sprintf('P%0*d',2,nParticipants(p)); %for use when navigating files
+    % Variables 
+    AllData = struct;
+    nSessions = 1:4;
+    % Directories
+    % Directory
+    dirBias = ('C:\Users\Experimenter\Documents\Experiments2018\Bias'); %subject to change depending on where you analyse
+    dirPP = [dirBias filesep ppID]; %participant directory
+    dirAna = [dirPP filesep 'Analysis' filesep];
+    dirVis = [dirAna 'Visual' filesep];
+    dirAnaAll = [dirBias filesep 'Analysis']; %directory for all analysis - here is where data should be saved from this file
+    
+    for i = 1:length(nSessions)
+        cd(dirPP)
+        session = sprintf('Session%0*d',2,nSessions(i));
+    end
+end
