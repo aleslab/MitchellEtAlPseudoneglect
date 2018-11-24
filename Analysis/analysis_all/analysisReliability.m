f9b3c2079fe9b3df9580cddfd6d69a49bd283d2d
Alex Mitchell <agm26@st-andrews.ac.uk>
2018-11-24 12:34:39 +0000

    Changing file organisation
    Two analysis files - one for reliability and one for response bias

---
+++ Analysis/analysis_all/analysisReliability.m
@@ -0,0 +1,10 @@
+%% Bias study group-level analyis
+%% AG.Mitchell - 24.11.18
+% This script takes the data from the MLB, LM and TRB tasks for each
+% participant
+% Data gets collated in a manner that's translatable to most statistical
+% platforms
+% Takes proportion error from MLB and TRB, and the threshold to 50%
+% (stimulus asymmetry when the participant thinks the line is bisected down
+% the midline) for LM tasks and uses Cronbach's alpha to assess whether the
+% data is consistent across sessions and modalities
