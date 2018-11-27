2360c8e2e5c158a5dbbe9f723d74862e7dfe9592
Alex Mitchell <agm26@st-andrews.ac.uk>
2018-11-27 19:25:28 +0000

    I DID IT
    Dakin plot is FINISHED... now to plot the sessions

--- Analysis/analysis_all/analysisReliability.m
+++ Analysis/analysis_all/analysisReliability.m
@@ -96,15 +96,27 @@ for p = 1:length(nParticipants)
         allData.sessions.CIhigh(p,i) = sessionlmCI(2);
         allData.sessions.lmStd(p,i) = sessionlmSD;
         % Manual line bisection
+        sessionmlb = allData.(sprintf('%s', session)).mlb.error(p,1);
+        sessionmlbSD = allData.(sprintf('%s', session)).mlb.error(p,1);
+        allData.sessions.mlb(p,i) = sessionmlb;
+        allData.sessions.mlbStd(p,i) = sessionmlbSD;
+        % Proportion error
         sessionmlb = allData.(sprintf('%s', session)).mlb.proportionError(p,1);
         sessionmlbSD = allData.(sprintf('%s', session)).mlb.proportionErrorStd(p,1);
         allData.sessions.mlbProp(p,i) = sessionmlb;
-        allData.sessions.mlbStd(p,i) = sessionmlbSD;
+        allData.sessions.mlbPropStd(p,i) = sessionmlbSD;
+        
         % Tactile rod bisection
+        sessiontrb = allData.(sprintf('%s', session)).trb.error(p,1);
+        sessiontrbSD = allData.(sprintf('%s', session)).trb.error(p,1);
+        allData.sessions.trb(p,i) = sessiontrb;
+        allData.sessions.trbStd(p,i) = sessiontrbSD;
+        % Proportion error
         sessiontrb = allData.(sprintf('%s', session)).trb.proportionError(p,1);
         sessiontrbSD = allData.(sprintf('%s', session)).trb.proportionErrorStd(p,1);
         allData.sessions.trbProp(p,i) = sessiontrb;
-        allData.sessions.trbStd(p,i) = sessiontrbSD;
+        allData.sessions.trbPropStd(p,i) = sessiontrbSD;
+        
     end
     
     %% Saving data for all modalities
@@ -115,12 +127,12 @@ for p = 1:length(nParticipants)
     modlmSD = std(allData.sessions.lmPSE(p,:));
     allData.modalities.data(p,1) = modlm; allData.modalities.sds(p,1) = modlmSD;
     % Manual line bisection
-    modmlb = mean(allData.sessions.mlbProp(p,:));
+    modmlb = mean(allData.sessions.mlb(p,:));
     modmlbSD = std(allData.sessions.mlbProp(p,:));
     allData.modalities.data(p,2) = modmlb; allData.modalities.sds(p,2) = modmlbSD;
     % Tactile rod bisection
-    modtrb = mean(allData.sessions.trbProp(p,:));
-    modtrbSD = std(allData.sessions.trbProp(p,:));
+    modtrb = mean(allData.sessions.trb(p,:));
+    modtrbSD = std(allData.sessions.trb(p,:));
     allData.modalities.data(p,3) = modtrb; allData.modalities.sds(p,3) = modtrbSD;
     
     
@@ -134,45 +146,34 @@ end
 % Any outliers identified through MATLAB - more than three scaled mean
 % absolute deviations away
 % Landmarks
-outlierLM = isoutlier(allData.sessions.lmPSE);
-outlierSumLM = sum(outlierLM,2); %sums the number of outliers across all participants
-% If the sum of outliers is >2, remove participant from the analysis
-removeLM = find(outlierSumLM > 2); %identifies paticipants that need removing
-
+outlierLM = isoutlier(allData.modalities.data(:,1));
 % Manual line bisection
-outlierMLB = isoutlier(allData.sessions.mlbProp);
-outlierSumMLB = sum(outlierMLB,2); %sums the number of outliers across all participants
-% If the sum of outliers is >2, remove participant from the analysis
-removeMLB = find(outlierSumMLB > 2); %identifies paticipants that need removing
-
+outlierMLB = isoutlier(allData.modalities.data(:,2));
 % Tactile rod bisection
-outlierTRB = isoutlier(allData.sessions.trbProp);
-outlierSumTRB = sum(outlierTRB,2); %sums the number of outliers across all participants
-% If the sum of outliers is >2, remove participant from the analysis
-removeTRB = find(outlierSumTRB > 2); %identifies paticipants that need removing
-
-removeAll = [removeLM; removeMLB; removeTRB]; 
-removeAll = sort(removeAll); %sorting so in participant order
+outlierTRB = isoutlier(allData.modalities.data(:,3));
+outlierAll = [outlierLM, outlierMLB, outlierTRB]; outlierSum = sum(outlierAll,2);
+remove = find(outlierSum > 0.1); %identifies paticipants that need removing
+remove = sort(remove); %sorting so in participant order
 
 % Removing the participants found to be outliers
 % Modalities and sessions
-for r = 1:length(removeAll)
+for r = 1:length(remove)
     % Identifying the outlier
-    allData.sessions.lmPSE(removeAll(r),:) = NaN;
-    allData.sessions.lmCIlow(removeAll(r),:) = NaN;
-    allData.sessions.lmCIhigh(removeAll(r),:) = NaN;
-    allData.sessions.lmStd(removeAll(r),:) = NaN;
-    allData.sessions.mlbProp(removeAll(r),:) = NaN;
-    allData.sessions.mlbPropStd(removeAll(r),:) = NaN;
-    allData.sessions.trbProp(removeAll(r),:) = NaN;
-    allData.sessions.trbPropStd(removeAll(r),:) = NaN;
+    allData.sessions.lmPSE(remove(r),:) = NaN;
+    allData.sessions.lmCIlow(remove(r),:) = NaN;
+    allData.sessions.lmCIhigh(remove(r),:) = NaN;
+    allData.sessions.lmStd(remove(r),:) = NaN;
+    allData.sessions.mlbProp(remove(r),:) = NaN;
+    allData.sessions.mlbPropStd(remove(r),:) = NaN;
+    allData.sessions.trbProp(remove(r),:) = NaN;
+    allData.sessions.trbPropStd(remove(r),:) = NaN;
     
     % Modalities across sessions
-    allData.modalities.data(removeAll(r),:) = NaN;
-    allData.modalities.sds(removeAll(r),:) = NaN;
+    allData.modalities.data(remove(r),:) = NaN;
+    allData.modalities.sds(remove(r),:) = NaN;
     
     % All means
-    allData.means.allPP(removeAll(r),:) = NaN;
+    allData.means.allPP(remove(r),:) = NaN;
 end
 % Removing outlier from matrix - modalities and sessions
 allData.sessions.lmPSE = allData.sessions.lmPSE(find(allData.sessions.lmPSE(:,1) > 0.00001 | ...
@@ -183,12 +184,12 @@ allData.sessions.CIlow= allData.sessions.CIlow(find(allData.sessions.CIlow(:,1) 
     allData.sessions.CIlow(:,1) < 0.00001),:);
 allData.sessions.lmStd= allData.sessions.lmStd(find(allData.sessions.lmStd(:,1) > 0.00001 | ...
     allData.sessions.lmStd(:,1) < 0.00001),:);
-allData.sessions.mlbProp= allData.sessions.mlbProp(find(allData.sessions.mlbProp(:,1) > 0.00001 | ...
-    allData.sessions.mlbProp(:,1) < 0.00001),:);
+allData.sessions.mlbProp= allData.sessions.mlb(find(allData.sessions.mlb(:,1) > 0.00001 | ...
+    allData.sessions.mlb(:,1) < 0.00001),:);
 allData.sessions.mlbStd= allData.sessions.mlbStd(find(allData.sessions.mlbStd(:,1) > 0.00001 | ...
     allData.sessions.mlbStd(:,1) < 0.00001),:);
-allData.sessions.trbProp= allData.sessions.trbProp(find(allData.sessions.trbProp(:,1) > 0.00001 | ...
-    allData.sessions.trbProp(:,1) < 0.00001),:);
+allData.sessions.trb= allData.sessions.trb(find(allData.sessions.trb(:,1) > 0.00001 | ...
+    allData.sessions.trb(:,1) < 0.00001),:);
 allData.sessions.trbStd= allData.sessions.trbStd(find(allData.sessions.trbStd(:,1) > 0.00001 | ...
     allData.sessions.trbStd(:,1) < 0.00001),:);
 
@@ -207,60 +208,27 @@ allData.means.allPP = allData.means.allPP(find(allData.means.allPP(:,1) > 0.0000
 % Across sessions
 % Mean for each
 allData.sessions.means.lmPSE = mean(allData.sessions.lmPSE);
-allData.sessions.means.mlbProp = mean(allData.sessions.mlbProp);
-allData.sessions.means.trbProp = mean(allData.sessions.trbProp);
+allData.sessions.means.mlb = mean(allData.sessions.mlb);
+allData.sessions.means.trb = mean(allData.sessions.trb);
 % Std for each
 allData.sessions.sds.lmPSE = std(allData.sessions.lmPSE);
-allData.sessions.sds.mlbProp = std(allData.sessions.mlbProp);
-allData.sessions.sds.trbProp = std(allData.sessions.trbProp);
+allData.sessions.sds.mlb = std(allData.sessions.mlb);
+allData.sessions.sds.trb = std(allData.sessions.trb);
 
 % Total for each modality
 allData.means.lmPSE(1) = mean(allData.sessions.means.lmPSE);
 allData.means.lmPSE(2) = std(allData.sessions.means.lmPSE);
-allData.means.mlbProp(1) = mean(allData.sessions.means.mlbProp);
-allData.means.mlbProp(2) = std(allData.sessions.means.mlbProp);
-allData.means.trbProp(1) = mean(allData.sessions.means.trbProp);
-allData.means.trbProp(2) = std(allData.sessions.means.trbProp);
+allData.means.mlb(1) = mean(allData.sessions.means.mlb);
+allData.means.mlb(2) = std(allData.sessions.means.mlb);
+allData.means.trb(1) = mean(allData.sessions.means.trb);
+allData.means.trb(2) = std(allData.sessions.means.trb);
 
 %Total
-allData.means.tot(1) = mean([allData.means.lmPSE(1), allData.means.mlbProp(1), ...
-    allData.means.trbProp(1)]);
-allData.means.tot(2) = std([allData.means.lmPSE(1), allData.means.mlbProp(1), ...
-    allData.means.trbProp(1)]);
+allData.means.tot(1) = mean([allData.means.lmPSE(1), allData.means.mlb(1), ...
+    allData.means.trb(1)]);
+allData.means.tot(2) = std([allData.means.lmPSE(1), allData.means.mlb(1), ...
+    allData.means.trb(1)]);
 
-% To compare across modalities all data needs to be roughly equal and
-% normalised 
-% Creating z-scores of all data for analysis and plotting
-for p = 1:length(allData.means.allPP)
-    modlmZ = (allData.sessions.lmPSE(p,:) - allData.means.lmPSE(1))/allData.means.lmPSE(2);
-    modmlbZ = (allData.sessions.mlbProp(p,:) - allData.means.mlbProp(1))/allData.means.mlbProp(2);
-    modtrbZ = (allData.sessions.trbProp(p,:) - allData.means.trbProp(1))/allData.means.trbProp(2);
-    
-    allData.modalities.zscore(p,1) = mean(modlmZ);
-    allData.modalities.zscoreSD(p,1) = std(modlmZ);
-    allData.modalities.zscore(p,2) = mean(modmlbZ);
-    allData.modalities.zscoreSD(p,2) = std(modmlbZ);
-    allData.modalities.zscore(p,3) = mean(modtrbZ);
-    allData.modalities.zscoreSD(p,3) = std(modtrbZ);
-    
-    % Mean all PP, zscore
-    allData.means.zscore.allPP(p,1) = mean([modlmZ, modmlbZ, modtrbZ]);
-    allData.means.zscore.allPP(p,2) = std([modlmZ, modmlbZ, modtrbZ]);
-end
-
-% Total for each modality
-allData.means.zscore.lm(1) = mean(allData.modalities.zscore(:,1));
-allData.means.zscore.lm(2) = std(allData.modalities.zscore(:,1));
-allData.means.zscore.mlb(1) = mean(allData.modalities.zscore(:,2));
-allData.means.zscore.mlb(2) = std(allData.modalities.zscore(:,2));
-allData.means.zscore.trb(1) = mean(allData.modalities.zscore(:,3));
-allData.means.zscore.trb(2) = std(allData.modalities.zscore(:,3));
-
-%Total
-allData.means.zscore.tot(1) = mean([allData.means.zscore.lm(1), allData.means.zscore.mlb(1), ...
-    allData.means.zscore.trb(1)]);
-allData.means.zscore.tot(2) = std([allData.means.zscore.lm(1), allData.means.zscore.mlb(1), ...
-    allData.means.zscore.trb(1)]);
 
 %% Data plots
 results = struct;
@@ -270,40 +238,30 @@ results.observers = 1:length(allData.means.allPP); %getting total number of obse
 % The idea for this plot was taken from the Dakin (2018, Vision Res) papert
 % figure 2a
 % Plot for mean bias in all participants
-% First - need to get lm data on the same scale as the mlb + trb data so...
-results.plotting.transformedlm = (allData.modalities.data(:,1)/100);
-results.plotting.lmmean(1) = mean(results.plotting.transformedlm); 
-results.plotting.lmmean(2) = std(results.plotting.transformedlm); 
-results.plotting.transformedMean(1) = mean([results.plotting.lmmean(1), allData.means.mlbProp(1), ...
-    allData.means.trbProp(1)]);
-results.plotting.transformedMean(2) = std([results.plotting.lmmean(1), allData.means.mlbProp(1), ...
-    allData.means.trbProp(1)]);
-results.plotting.transformedMeanAll(:,1) = mean([results.plotting.transformedlm, allData.modalities.data(:,2),...
-    allData.modalities.data(:,3)],2);
-results.plotting.transformedMeanAll(:,2) = std([results.plotting.transformedlm, allData.modalities.data(:,2),...
-    allData.modalities.data(:,3)],0,2);
 % First - need to order by bias (left -> right)
 % Need to place all data in the same matrix to do this, then sort entire
 % matrix by mean bias
 results.plotting.modalities(:,1) = results.observers;
-results.plotting.modalities(:,2) = results.plotting.transformedMeanAll(:,1); %mean all data
-results.plotting.modalities(:,3) = results.plotting.transformedlm; %lm
+results.plotting.modalities(:,2) = allData.means.allPP(:,1); %mean all data
+results.plotting.modalities(:,3) = allData.modalities.data(:,1); %lm
 results.plotting.modalities(:,4) = allData.modalities.data(:,2); %mlb
-results.plotting.modalities(:,5) = allData.modalities.data(:,3); %trb
+results.plotting.modalities(:,5) = allData.modalities.data(:,3); %trb.
+results.plotting.modalities(:,6) = allData.means.allPP(:,2); %std all data (across modalities)
 % Sorting the matrix by mean bias
 results.plotting.modalities = sortrows(results.plotting.modalities, 2);
-results.plotting.modalities(:,6) = results.observers; %observers not sorted by mean bias for use with plotting - organisation of data
+results.plotting.modalities(:,7) = results.observers; %observers not sorted by mean bias for use with plotting - organisation of data
 
 % standard deviation values for shading
-SDpt5 = results.plotting.transformedMean(2)*0.5;
-SD2 = results.plotting.transformedMean(2)*2;
+SDpt5 = allData.means.tot(2)*0.5;
+SD2 = allData.means.tot(2)*2;
+SDall = results.plotting.modalities(:,6);
 
 cd(dirAnaAll);
 % Making initial mean error (all modalities) plot
-figure()
-scatter(results.plotting.modalities(:,6), results.plotting.modalities(:,2), ...
+figure('units', 'centimeters', 'Position', [5 3 18 12])
+scatter(results.plotting.modalities(:,7), results.plotting.modalities(:,2), ...
     'filled', '^'); % mean task data
-ylim([-0.1 0.1]);
+ylim([-3 3]);
 line('XData', [0 length(results.observers)], 'YData', [0, 0], 'LineStyle', '-', ...
     'LineWidth', 0.5, 'Color', 'k'); %midpoint
 % Adding SD shaded area
@@ -314,27 +272,34 @@ hold on
 createShadedRegion(xVal, shadedVal, (shadedVal - SDpt5), (shadedVal + SDpt5),':','color', [0.1 0.1 0.1]);
 hold on
 createShadedRegion(xVal, shadedVal, (shadedVal - SD2), (shadedVal + SD2),':','color', [0.5 0.5 0.5]);
+% Adding error bars to the mean data
+hold on
+errorbar(results.plotting.modalities(:,7), results.plotting.modalities(:,2), SDall, 'LineStyle', 'none',...
+    'LineWidth', 0.7, 'Color', [0 0 0], 'CapSize', 0);
 % Making it prettier
 set(ax, 'FontSize', 12);
 set(ax, 'XTick', results.observers);
 xlabel('Observers'); ylabel('Bias as a proportion of line length');
-figFileName = strcat('meanBias', '.pdf');
-saveas(gcf, figFileName);
+pdfFileName = strcat('meanBias', '.pdf');
+saveas(gcf, pdfFileName);
 
-% Making plot to show bias in all modalities
-figure()
-scatter(results.plotting.modalities(:,6), results.plotting.modalities(:,2), ...
-    'filled', '^'); % mean task data
+%% Making plot to show bias in all modalities
+pdfFileName = strcat('meanBias_modalities', '.pdf');
+pngFileName = strcat('meanBias_modalities', '.png');
+
+figure('units', 'centimeters', 'Position', [5 3 18 12])
 hold on
-scatter(results.plotting.modalities(:,6), results.plotting.modalities(:,3), ...
-    'filled', 'o'); % landmark task data
+scatter(results.plotting.modalities(:,7), results.plotting.modalities(:,3), ...
+    'filled', 'o', 'MarkerFaceColor', [0 0 0.4]); % landmark task data
 hold on
-scatter(results.plotting.modalities(:,6), results.plotting.modalities(:,4), ...
-    'filled', 'o'); % mlb task data
+scatter(results.plotting.modalities(:,7), results.plotting.modalities(:,4), ...
+    'filled', 'o', 'MarkerFaceColor', [0 0 0.8]); % mlb task data
 hold on
-scatter(results.plotting.modalities(:,6), results.plotting.modalities(:,5), ...
-    'filled', 'o'); % trb task data
-ylim([-0.1 0.1]);
+scatter(results.plotting.modalities(:,7), results.plotting.modalities(:,5), ...
+    'filled', 'o', 'MarkerFaceColor', [0.6 0.6 1]); % trb task data
+scatter(results.plotting.modalities(:,7), results.plotting.modalities(:,2), ...
+    'filled', '^', 'MarkerEdgeColor', [0.3 0 0.3]); % mean task data
+ylim([-3 3]);
 line('XData', [0 length(results.observers)], 'YData', [0, 0], 'LineStyle', '-', ...
     'LineWidth', 0.5, 'Color', 'k'); %midpoint
 % Adding SD shaded area
@@ -345,12 +310,24 @@ hold on
 createShadedRegion(xVal, shadedVal, (shadedVal - SDpt5), (shadedVal + SDpt5),':','color', [0.1 0.1 0.1]);
 hold on
 createShadedRegion(xVal, shadedVal, (shadedVal - SD2), (shadedVal + SD2),':','color', [0.5 0.5 0.5]);
+% Adding error bars to the mean data
+hold on
+errorbar(results.plotting.modalities(:,7), results.plotting.modalities(:,2), SDall, 'LineStyle', 'none',...
+    'LineWidth', 0.7, 'Color', [0 0 0], 'CapSize', 0);
 % Making it prettier
 set(ax, 'FontSize', 12);
 set(ax, 'XTick', results.observers);
 xlabel('Observers'); ylabel('Bias as a proportion of line length');
-figFileName = strcat('meanBias', '.pdf');
-saveas(gcf, figFileName);
+legend('Landmarks', 'MLB', 'TRB', 'Mean', [120 280 0.2 0.1]);
+% Adding text to define bias grouping
+leftDim = [0.17 0.13 0.25 0.045];
+rightDim = [0.77 0.13 0.13 0.045]; midDim = [0.43 0.13 0.33 0.045];
+annotation('textbox', leftDim, 'String', 'Left', 'FontSize', 8); %'Color', [0.2 0.2 0.2]);
+annotation('textbox', rightDim, 'String', 'Right', 'FontSize', 8); %'Color', [0.7 0.7 0.7]);
+annotation('textbox', midDim, 'String', 'Unspecified', 'FontSize', 8);  %'Color', [0.45 0.45 0.45]);
+% saving as both pdf and png for ease
+saveas(gcf, pdfFileName);
+saveas(gcf, pngFileName);
 
 %set(gca, 'Xtick', results.plotting.modalities(:,1));
 % need to 'prettyfy', add correct labels, add group info, change axes, standard deviation, midline bar
@@ -363,12 +340,11 @@ type = 'C-k'; %type of ICC used - 2-k, 2-way fixed effects
 [results.sessions.lm.r, results.sessions.lm.bound(1), results.sessions.lm.bound(2), results.sessions.lm.F,...
     results.sessions.lm.df(1), results.sessions.lm.df(2), results.sessions.lm.p] = ICC(allData.sessions.lmPSE, type);
 [results.sessions.mlb.r, results.sessions.mlb.bound(1), results.sessions.mlb.bound(2), results.sessions.mlb.F,...
-    results.sessions.mlb.df(1), results.sessions.mlb.df(2), results.sessions.mlb.p] = ICC(allData.sessions.mlbProp, type);
+    results.sessions.mlb.df(1), results.sessions.mlb.df(2), results.sessions.mlb.p] = ICC(allData.sessions.mlb, type);
 [results.sessions.trb.r, results.sessions.trb.bound(1), results.sessions.trb.bound(2), results.sessions.trb.F,...
-    results.sessions.trb.df(1), results.sessions.trb.df(2), results.sessions.trb.p] = ICC(allData.sessions.trbProp, type);
+    results.sessions.trb.df(1), results.sessions.trb.df(2), results.sessions.trb.p] = ICC(allData.sessions.trb, type);
 
 % Reliability across modalities
-modalitiesData = [results.plotting.transformedlm, allData.modalities.data(:,2),  allData.modalities.data(:,3)];
 [results.modalities.all.r, results.modalities.all.bound(1), results.modalities.all. bound(2), results.modalities.all.F,...
     results.modalities.all.df(1), results.modalities.all.df(2), results.modalities.all.p] = ICC(allData.modalities.data, type);
 
