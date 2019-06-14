64d335a0578691455225e20add7de059f184e6b8
Alex <alexandra.mitchell@ed.ac.uk>
2019-06-14 10:29:28 +0100

    Creating histogram plots
    To estimate spread of individual differences in data

---
+++ Analysis/individualDifferences/histograms.m
@@ -0,0 +1,12 @@
+%% AG. Mitchell - 14.06.19
+%% Plotting individual participant data into a histogram
+% Do different tasks show different spread of data?
+% Look acros individual tasks and also individual testing sessions
+
+% Take data from the reliabilityAnalysis.mat file (extracted using
+% ReliabilityAnalysis.m script)
+
+%% Data
+%% Individual tasks
+%% Individual sessions - each task
+%% Save and close
