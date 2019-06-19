f0ec0a4b6c5840015991512763ab2418a7439f54
Alex <alexandra.mitchell@ed.ac.uk>
2019-06-19 10:51:44 +0100

    Correlation script
    Exploratory analysis cnt
    Correlations between data from LM task and LM2AFC task - are there any trends? Can we model something

---
+++ Analysis/individualDifferences/correlations.m
@@ -0,0 +1,9 @@
+%% AG.Mitchell - 19.06.19
+%% Exploratory analysis
+% Investigating correlations between standard bisection and 2AFC tasks to
+% see whether data from one can predict the other in the same participant
+% Landmark/line bisection correlated with landmark 2AFC, tactile rod
+% bisection correlated with tactile rod 2AFC
+
+%% Directories and variables
+%% Landmark vs. LM2AFC
