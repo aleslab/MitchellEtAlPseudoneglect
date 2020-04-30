df3e3dcfd0bfac59851e38345e3c383e66289687
Alex <alexandra.mitchell@ed.ac.uk>
2020-04-30 12:43:54 +0100

    Update analysis_handUsed.m
    Reviewer comments: hand used analysis, setting paths and directories

--- Analysis/analysis_all/analysis_handUsed.m
+++ Analysis/analysis_all/analysis_handUsed.m
@@ -6,10 +6,29 @@
 % Addressing reviewer 2 comments in Neuropsychologia re-submission (1.1)
 
 %% Setting paths
+nParticipants = [1:19,21:24,26:30];
+for p = 1:length(nParticipants)
+    % Directory
+    dirBias = ('M:\Alex_Files\Experiments\Bias'); %subject to change depending on where you analyse
+    
+    ppID = sprintf('P%0*d',2,nParticipants(p)); %for use when navigating files
+    matfilename = sprintf('%s_visualanalysisStart.mat', ppID);
+    nSessions = 1:4; %vector number of sessions each participant does
+    
+    % individual participant directory, analysis
+    dirPP = [dirBias filesep 'Data' filesep ppID filesep 'Analysis']; %participant directory
+    dirVis = [dirPP filesep 'Visual'];
+    dirTac = [dirPP filesep 'Tactile'];
+    cd(dirPP)
+    
 %% MLB Extracting data
+    % for MLB - go to visual folder
+    
+%% TRB Extracting data
+end
 %% Group averages
 %% Plotting data
-%% TRB Extracting data
+
 %% Group averages
 %% Plotting data
 
