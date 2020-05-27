f711b5d7e434bfa68dc53fbdebcefd37b092a3f4
Alex <alexandra.mitchell@ed.ac.uk>
2020-05-27 12:45:21 +0100

    Update analysis_LMlineContrast.m
    Setting up pathways for analysis

--- Analysis/analysis_all/analysis_LMlineContrast.m
+++ Analysis/analysis_all/analysis_LMlineContrast.m
@@ -5,3 +5,29 @@
 % top left = black; top left = white) to see if visual illusion may be
 % biasing response 
 
+%% Setting paths
+clear all
+nParticipants = [1:19,21:24,26:30];
+% Directory
+dirBias = ('M:\Alex_Files\Experiments\Bias'); %subject to change depending on where you analyse
+dirAna = ('M:\Alex_Files\Experiments\Bias\Analysis');
+
+%% Looping through participants and getting data
+% Average across sessions
+% Individual sessions (1-4)
+for p = 1:length(nParticipants)
+    ppID = sprintf('P%0*d',2,nParticipants(p)); %for use when navigating files
+    visualFilename = sprintf('%s_visualanalysisStart.mat', ppID);
+    matfilename = ('LMcontrast_analysis.mat');
+    
+    nSessions = 1:4; %vector number of sessions each participant does
+    
+    % individual participant directory
+    dirPP = [dirBias filesep 'Data' filesep ppID filesep 'Analysis']; %participant directory
+    dirVis = [dirPP filesep 'Visual'];
+    cd(dirVis)
+    % load file
+    load(visualFilename)
+    
+    
+end
