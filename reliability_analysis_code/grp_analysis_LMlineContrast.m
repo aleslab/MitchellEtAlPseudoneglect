3a9e4053ac21d050ca060ba57907acdd709215e1
Justin Ales <jma23@st-andrews.ac.uk>
2020-08-26 21:38:17 +0100

    changed some comments

--- reliability_analysis_code/grp_analysis_LMlineContrast.m
+++ reliability_analysis_code/grp_analysis_LMlineContrast.m
@@ -1,6 +1,5 @@
 %% AG.Mitchell - 27.05.20
 %% Analysis script looking at differences in response to different line contrasts for the landmark task
-% As requested by reviewer 1 in Neuropsychologia resubmission (1.1)
 % Script runs through right side longer responses for each line contrast (
 % top left = black; top left = white) to see if visual illusion may be
 % biasing response 
